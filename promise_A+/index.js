d16fba0bc4752cb7102ae2d57bb4b710e06db0d1
darki <125730892@qq.com>
2019-03-17 23:24:56 +0800

    promise fulfilled

--- promise_A+/index.js
+++ promise_A+/index.js
@@ -4,4 +4,6 @@ let p = new MyPromise((resolve, reject) => {
   resolve('同步执行')
 }).then((v) => {
   console.log(v)
+}).then((v) => {
+  console.log("v" + v)
 })
--- promise_A+/promise.js
+++ promise_A+/promise.js
@@ -1,28 +1,108 @@
+// 定义三种状态
+const PENDING   = 'pending'
+const FULFILLED = 'fulfilled'
+const REJECTED  = 'rejectend'
+
 function MyPromise(fn) {
-  let self          = this;         // 固定 current this
-  self.value        = null;         // 成功返回值
-  self.error        = null;         // 失败信息
-  self.onFulfilled  = null;         // succeed callback
-  self.onRejected   = null;         // failure callback
+  let self                    = this      // 固定 current this
+  self.value                  = null      // 成功返回值
+  self.error                  = null      // 失败信息
+  self.status                 = PENDING   // 初始化状态
+  self.onFulfilledCallbacks   = []        // succeed callback
+  self.onRejectedCallbacks    = []        // failure callback
 
   function resolve(value) {
-    setTimeout(() => {
-      self.value = value;
-      self.onFulfilled(self.value);   // resolve执行成功时回调
-    })
+    if (self.status === PENDING) {
+      setTimeout(() => {
+        self.status = FULFILLED
+        self.value = value
+        self.onFulfilledCallbacks.forEach((callback) => callback(self.value))
+      })
+    }
   }
 
   function rejected(error) {
-    setTimeout(() => {
-      self.error = error;
-      self.onRejected(self.value);    // reject时执行失败回调
-    })
+    if (self.status === PENDING) {
+      setTimeout(() => {
+        self.status = REJECTED
+        self.error = error
+        self.onRejectedCallbacks.forEach((callback) => callback(self.value))
+      })
+    }
   }
   fn(resolve, rejected)
 }
 MyPromise.prototype.then = function(onFulfilled, onRejected) {
-  this.onFulfilled = onFulfilled
-  this.onRejected = onRejected
+  const self = this
+  let bridgePromise
+  // 防止没有传入回调方法的情况
+  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+  onRejected = typeof onRejected === 'function' ? onRejected : error => { throw error }
+  if (self.status === FULFILLED) {
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          let x = onFulfilled(self.value)
+          resolvePromise(bridgePromise, x, resolve, reject)
+        } catch(e) {
+          reject(e)
+        }
+      });
+    })
+  }
+  if (self.status === REJECTED) {
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      setTimeout(() => {
+        try {
+          let x = onRejected(self.error)
+          resolvePromise(bridgePromise, x, resolve, reject)
+        } catch(e) {
+          reject(e)
+        }
+      });
+    })
+  }
+  if (self.status === PENDING) {
+    return bridgePromise = new MyPromise((resolve, reject) => {
+      self.onFulfilledCallbacks.push((value) => {
+        try {
+          let x = onFulfilled(value)
+          resolvePromise(bridgePromise, x, resolve, reject)
+        } catch(e) {
+          reject(e)
+        }
+      })
+      self.onRejectedCallbacks.push((error) => {
+        try {
+          let x = onRejected(error);
+          resolvePromise(bridgePromise, x, resolve, reject)
+        } catch(e) {
+          reject(e)
+        }
+      })
+    })
+  }
+  return this
+}
+
+MyPromise.prototype.catch = function(onRejected) {
+  return this.then(null, onRejected)
+}
+
+function resolvePromise(bridgePromisem, x, resolve, reject) {
+  if (x instanceof MyPromise) {
+    if(x.status === PENDING) {
+      x.then(y => {
+        resolvePromise(bridgePromisem, y, resolve, reject)
+      }, error => {
+        reject(error)
+      })
+    }else {
+      x.then(resolve, reject)
+    }
+  } else {
+    resolve(x)
+  }
 }
 
 module.exports = MyPromise;
