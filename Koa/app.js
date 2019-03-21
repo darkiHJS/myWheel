66ba67798a89db6f405305beb7c7fed70f20a9be
darki <125730892@qq.com>
2019-03-21 22:59:53 +0800

    完成了Koa的中间件

--- Koa/app.js
+++ Koa/app.js
@@ -3,8 +3,20 @@ const app = new Koa()
 
 // app.context.XXXX 可以对ctx对象进行扩展 可以挂在一些常用放，egg就是这么干的
 
-app.use(async ctx => {
-  ctx.body = 'hello,' + ctx.query.name
+app.use(async (ctx, next) => {
+  console.log(1)
+  next()
+  console.log(6)
+})
+app.use(async (ctx, next) => {
+  console.log(2)
+  next()
+  console.log(5)
+})
+app.use(async (ctx, next) => {
+  console.log(3)
+  next()
+  console.log(4)
 })
 
 app.listen(3000, () => {
