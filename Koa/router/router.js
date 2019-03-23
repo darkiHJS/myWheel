667c2460450d57ac765f821f366584152b5bf037
胡嘉仕 <125730892@qq.com>
2019-03-23 16:14:53 +0800

    koa-router

--- Koa/router/router.js
+++ Koa/router/router.js
@@ -1,13 +1,13 @@
 const path = require('path')
-
+const methods = require('http').METHODS
 // private
 const PREFIX = Symbol('router.prefix')
-const SERVER = Symbol('router.server')
+const OPTIONS = Symbol('router.options')
 const MIDDLEWARES = Symbol('router.middlewares');
 const VERB = Symbol('router#verb')
 
-function Router(server) {
-  this[SERVER] = server
+function Router(opts) {
+  this[OPTIONS] = opts
   this[PREFIX] = '/' 
   this[MIDDLEWARES] = []
 }
@@ -28,11 +28,28 @@ Router.prototype[VERB] = function(method, router, ...callbacks) {
   : path.resolve(this[PREFIX], router)
   
   callbacks = callbacks.map(callback => {
-    if
-  })
   
-
+  })
 }
+
+methods.forEach(function (method){
+  Router.prototype[method] = function(name, path, middleware) {
+    var middleware
+    // 防止name没有被传入的情况发生
+    if(typeof path === 'string' || path instanceof RegExp) {
+      middleware = Array.prototype.slice.call(arguments, 2)
+    } else {
+      middleware = Array.prototype.slice.call(arguments, 1)
+      path = name
+      name = null
+    }
+    // 注册路由
+    this.register(path, [method], middleware, {
+      name
+    })
+    return this
+  }
+})
 
 
 module.exports = Router
--- Koa/test/index.js
+++ Koa/test/index.js
@@ -1,4 +1,3 @@
-const path = require('path')
+const http = require('http')
 
-var str = path.resolve('./','./index.js')
-console.log(str)
+console.log(http.METHODS)
